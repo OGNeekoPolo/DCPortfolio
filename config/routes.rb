f6aaa9d76b94b3313835737b11df4360ee4c3ab8
OGNeekoPolo <nktrabue@gmail.com>
2018-02-17 20:51:28 -0500

    Added Portfolios Items via resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  resources :portfolios
+
   resources :blogs
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
