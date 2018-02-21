371bc71281906807c2ae502758563da3cf34b7cf
OGNeekoPolo <nktrabue@gmail.com>
2018-02-21 00:49:59 -0500

    Added Portfolio index view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :portfolios
 
   resources :blogs
+
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
