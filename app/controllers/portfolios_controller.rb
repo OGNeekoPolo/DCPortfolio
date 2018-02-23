a0f0dc8ac1085602963a6479ad44a88343a708d1
OGNeekoPolo <nktrabue@gmail.com>
2018-02-22 23:39:04 -0500

    Added Show page for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,11 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:edit, :update, :destroy, :show]
+
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def show; end
 
   def new
     @portfolio_item = Portfolio.new
@@ -16,7 +20,26 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @portfolio_item.update(portfolio_params)
+      redirect_to portfolios_path, notice: 'Portfolio was successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    redirect_to portfolios_path
+  end
+
   private
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
