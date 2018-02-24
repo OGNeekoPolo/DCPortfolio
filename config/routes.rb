56ca326df67de5638ebf45408b4cdda94084cfe9
OGNeekoPolo <nktrabue@gmail.com>
2018-02-24 17:46:17 -0500

    Implemented Custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: :portfolio_show
 
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   root 'pages#home'
 end
