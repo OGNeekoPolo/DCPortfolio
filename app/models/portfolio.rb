319f3fad5d4faf7a6855dfca85c547137e0ada63
OGNeekoPolo <nktrabue@gmail.com>
2018-02-25 18:46:33 -0500

    Added defaults to Portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,24 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  # Custom scope
+  # Custom scope also has alternate syntax
+  # scope :angular, -> { where(subtitle: 'Ruby on Rails') }
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  def self.ruby_on_rails
+    where(subtitle: 'Ruby on Rails')
+  end
+
+  # Callback
+  after_initialize :set_defaults
+
+  # Sets default image values for newly initialized Portfolio item
+  # Example: @portfolio_item = Portfolio.new
+  def set_defaults
+    self.main_image ||= 'http://via.placeholder.com/600x400'
+    self.thumb_image ||= 'http://via.placeholder.com/350x200'
+  end
 end
