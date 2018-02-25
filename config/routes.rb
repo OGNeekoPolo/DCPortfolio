e59784127cef60d86b6d691147b15f3e423991ee
OGNeekoPolo <nktrabue@gmail.com>
2018-02-25 17:10:03 -0500

    Added validations to models

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: :portfolio_show
 
-
   resources :blogs do
     member do
       get :toggle_status
