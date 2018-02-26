144b30ec8c1b8ec3fbe2b13fc1396ede3ab24a38
OGNeekoPolo <nktrabue@gmail.com>
2018-02-25 19:59:21 -0500

    Added concerns for models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225221918) do
+ActiveRecord::Schema.define(version: 20180225235408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180225221918) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "percent_completed"
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
