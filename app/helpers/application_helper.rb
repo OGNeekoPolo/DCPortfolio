8bd36dfeb5430cf4dfd6025840a9978b72072255
OGNeekoPolo <nktrabue@gmail.com>
2018-03-04 01:01:19 -0500

    Added styles for application layout page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,12 @@ module ApplicationHelper
 
   def login_helper
     if current_user.is_a?(GuestUser)
-      (link_to 'Register',
-        new_user_registration_path, class: 'btn btn-warning') +
+      (link_to 'Register', new_user_registration_path) +
         '<br/>'.html_safe +
-        (link_to 'Login',
-          new_user_session_path, class: 'btn btn-success')
+        (link_to 'Login', new_user_session_path)
     else
       link_to 'Logout', destroy_user_session_path, method: :delete,
-              data: {confirm: 'Are you sure you want to logout?'},
-              class: 'btn btn-danger'
+              data: {confirm: 'Are you sure you want to logout?'}
     end
   end
 
