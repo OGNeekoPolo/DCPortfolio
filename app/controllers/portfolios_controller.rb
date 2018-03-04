1a685c35ce8df3a83c7418ef742d83c9f71f3ad2
OGNeekoPolo <nktrabue@gmail.com>
2018-03-03 23:17:23 -0500

    Added Authorization engine using PeterGate

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:edit, :update, :destroy, :show]
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   layout 'portfolio'
 
   def index
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
 
   def login_helper
-    if current_user.is_a?(User)
-      link_to 'Logout', destroy_user_session_path, method: :delete,
-                    data: {confirm: 'Are you sure you want to logout?'},
-                    class: 'btn btn-danger'
-    else
+    if current_user.is_a?(GuestUser)
       (link_to 'Register',
-               new_user_registration_path, class: 'btn btn-warning') +
+        new_user_registration_path, class: 'btn btn-warning') +
         '<br/>'.html_safe +
         (link_to 'Login',
-               new_user_session_path, class: 'btn btn-success')
+          new_user_session_path, class: 'btn btn-success')
+    else
+      link_to 'Logout', destroy_user_session_path, method: :delete,
+              data: {confirm: 'Are you sure you want to logout?'},
+              class: 'btn btn-danger'
     end
   end
 
