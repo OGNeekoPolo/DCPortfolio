e3711ff4704831116948ddc023e63e2ebafafbde
OGNeekoPolo <nktrabue@gmail.com>
2018-03-15 21:26:51 -0400

    Integrated CarrierWave and S3 for image hosting

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,6 +5,8 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
   # Custom scope
   # Custom scope also has alternate syntax
   # scope :angular, -> { where(subtitle: 'Ruby on Rails') }
