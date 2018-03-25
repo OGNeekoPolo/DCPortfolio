2a707c62e3bd4c4cad9dc9e00d8424c3d02fb560
OGNeekoPolo <nktrabue@gmail.com>
2018-03-25 10:20:33 -0400

    Wired up Topics to Blogs w collection dropdown

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,13 @@
 class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
+
   extend FriendlyId
   friendly_id :title, use: :slugged
+
   belongs_to :topic
   has_many :comments, dependent: :destroy
-  validates_presence_of :title, :body
+
+  validates_presence_of :title, :body, :topic_id
 
   def self.recent
     order('created_at DESC')
