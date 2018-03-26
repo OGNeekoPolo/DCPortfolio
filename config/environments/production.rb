7c43772aac6c704da2d7fea536758efb72b62ebd
OGNeekoPolo <nktrabue@gmail.com>
2018-03-26 01:04:53 -0400

    Configured production environment for ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://niktrabue.herokuapp.com', 'http://niktrabue.herokuapp.com']
+  config.action_cable.url = "wss://niktrabue.herokuapp.com/cable"
 end
